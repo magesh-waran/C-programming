4fe9a0481e397ff9407ceaa1d7ddeb09c5af3a10
SasiKumarR0786 <66399785+SasiKumarR0786@users.noreply.github.com>
2020-08-11 00:06:41 +0530

    Create DecimalToHexaDecimalUsingFormatSpecifier.c

---
+++ DecimalToHexaDecimalUsingFormatSpecifier.c
@@ -0,0 +1,12 @@
+/*EASIEST WAY TO CONVERT DECIMAL TO HEXADECIMAL
+input:
+15
+output:
+F*/
+
+#include<stdio.h>
+int main(){
+int decimal;
+scanf("%d",&decimal);
+printf("%X",decimal);//it will print hexadecimal value
+}
