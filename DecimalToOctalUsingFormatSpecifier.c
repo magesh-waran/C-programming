fec1bc9ba89955387f4d9243402e7fa0d1a032a8
SasiKumarR0786 <66399785+SasiKumarR0786@users.noreply.github.com>
2020-08-11 00:03:30 +0530

    Update and rename OctalToDecimalUsingFormatSpecifier.c to DecimalToOctalUsingFormatSpecifier.c

---
+++ DecimalToOctalUsingFormatSpecifier.c
@@ -0,0 +1,12 @@
+/*EASIEST WAY TO CONVERT DECIMAL TO OCTAL
+input:
+11
+output:
+13*/
+
+#include<stdio.h>
+int main(){
+int decimal;
+scanf("%d",&decimal);
+printf("%o",decimal);//it will print octal value
+}
